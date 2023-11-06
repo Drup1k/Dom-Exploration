f374a2a7648a6bc759e10f2382c80871164c67d8
Andrej <andrej.zhdkv@gmail.com>
2023-11-06 15:08:57 +0100

    add navigation DOM ex

--- create.js
+++ create.js
@@ -41,3 +41,81 @@ function getRandomColor() {
     // Append the section to the article
     article.appendChild(section);
   });
+
+
+//   const backgroundColor = "rgb(" + red + "," + green + "," + bleu +")";
+//   SectionElement.style.backgroundColor - backgroundColor;
+
+//   luminance = 0.2126 * (red / 255) + 0.7152 * (green / 255) + 0.0722 * (blue / 255);
+
+//   if (luminance > 0.5) {
+    // const learnerParagraph = document.createElement(tagName:"p");
+    // learnerParagraph.style.color = "black";
+    // learnerParagraph.textContent = learners;
+//   }
+
+//   console.log(luminance) 
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Function to check if the color is dark
+    function isDarkColor(bgColor) {
+      // Strip any leading #
+      if (bgColor.charAt(0) === '#') {
+        bgColor = bgColor.slice(1);
+      }
+      
+      // Parse the hex color
+      var r = parseInt(bgColor.substr(0, 2), 16);
+      var g = parseInt(bgColor.substr(2, 2), 16);
+      var b = parseInt(bgColor.substr(4, 2), 16);
+  
+      // Calculate the luminance
+      var uicolors = [r / 255, g / 255, b / 255];
+      var c = uicolors.map((col) => {
+        if (col <= 0.03928) {
+          return col / 12.92;
+        } else {
+          return Math.pow((col + 0.055) / 1.055, 2.4);
+        }
+      });
+      var luminance = 0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2];
+      
+      // Return true if the color is dark
+      return luminance < 0.179;
+    }
+  
+    // Function to set the text color based on background color
+    function setTextColorForBg(section) {
+      var bgColor = getComputedStyle(section).backgroundColor;
+      
+      // Convert rgb to hex if needed
+      var hexColor = rgbToHex(bgColor);
+      
+      if (isDarkColor(hexColor)) {
+        section.style.color = 'white';
+      }
+    }
+  
+    // Convert rgb color to hex
+    function rgbToHex(rgb) {
+      if (rgb.indexOf('#') === 0) return rgb;
+      
+      var parts = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+      delete(parts[0]);
+      
+      for (var i = 1; i <= 3; ++i) {
+        parts[i] = parseInt(parts[i]).toString(16);
+        if (parts[i].length == 1) parts[i] = '0' + parts[i];
+      }
+      
+      return '#' + parts.join('');
+    }
+  
+    // Select all sections
+    var sections = document.querySelectorAll('section');
+  
+    // Loop through the sections and set the text color if the background is dark
+    sections.forEach(function(section) {
+      setTextColorForBg(section);
+    });
+  });
---
+++ nav-dom-str.js
@@ -0,0 +1,37 @@
+// document.addEventListener('DOMContentLoaded', function() {
+    // Select the <ol> element
+    var list = document.querySelector('ol');
+  
+    // Select the last <li> element which is the last child of the <ol>
+    var lastItem = list.lastElementChild;
+  
+    // Move the last <li> to be the first child of the <ol>
+    if (lastItem) {
+      list.insertBefore(lastItem, list.firstElementChild);
+    }
+//   });
+
+
+    // Select the sections
+    var sections = document.querySelectorAll('main > section');
+  
+    // Check if we have at least three sections
+    if(sections.length >= 3) {
+      // The second section is at index 1 and the third at index 2
+      var secondSection = sections[1];
+      var thirdSection = sections[2];
+  
+      // Select the <h2> elements within those sections
+      var secondSectionHeading = secondSection.querySelector('h2');
+      var thirdSectionHeading = thirdSection.querySelector('h2');
+  
+      // Clone the headings to swap them to avoid DOM issues
+      var clonedSecondSectionHeading = secondSectionHeading.cloneNode(true);
+      var clonedThirdSectionHeading = thirdSectionHeading.cloneNode(true);
+  
+      // Replace the headings
+      secondSection.replaceChild(clonedThirdSectionHeading, secondSectionHeading);
+      thirdSection.replaceChild(clonedSecondSectionHeading, thirdSectionHeading);
+    }
+  
+  
